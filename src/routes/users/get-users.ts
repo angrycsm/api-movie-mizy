9de4718d3f828713b11a1bf482b4f6bcc79f8cc0
Gabriel Moraes <121532661+angrycsm@users.noreply.github.com>
2024-05-01 18:42:38 +0000

    re-indent two files from users folder

--- src/routes/users/get-users.ts
+++ src/routes/users/get-users.ts
@@ -4,31 +4,31 @@ import { FastifyRequest, FastifyReply, FastifyInstance } from "fastify";
 import { prisma } from "/home/runner/mm-backend/src/lib/prisma";
 
 export async function getUsers(app: FastifyInstance) {
-	app.withTypeProvider<ZodTypeProvider>().get(
-		"/users",
-		{
-			schema: {
-				response: {
-					200: z.object({
-						users: z.array(
-							z.object({
-								id: z.number(),
-								email: z.string().email(),
-								name: z.string().min(3),
-							}),
-						),
-					}),
-				},
-			},
-		},
-		async (request: FastifyRequest, reply: FastifyReply) => {
-			const users = await prisma.user.findMany();
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/users",
+    {
+      schema: {
+        response: {
+          200: z.object({
+            users: z.array(
+              z.object({
+                id: z.number(),
+                email: z.string().email(),
+                name: z.string().min(3),
+              }),
+            ),
+          }),
+        },
+      },
+    },
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const users = await prisma.user.findMany();
 
-			if (users.length === 0) {
-				throw new Error("No users found");
-			}
+      if (users.length === 0) {
+        throw new Error("No users found");
+      }
 
-			return reply.status(200).send({ users });
-		},
-	);
+      return reply.status(200).send({ users });
+    },
+  );
 }
--- src/routes/users/register-user.ts
+++ src/routes/users/register-user.ts
@@ -4,43 +4,43 @@ import { FastifyRequest, FastifyReply, FastifyInstance } from "fastify";
 import { prisma } from "/home/runner/mm-backend/src/lib/prisma";
 
 export async function registerUser(app: FastifyInstance) {
-	app.withTypeProvider<ZodTypeProvider>().post(
-		"/signup",
-		{
-			schema: {
-				body: z.object({
-					email: z.string().email("This is not a valid email"),
-					name: z.string().min(3),
-				}),
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/signup",
+    {
+      schema: {
+        body: z.object({
+          email: z.string().email("This is not a valid email"),
+          name: z.string().min(3),
+        }),
 
-				response: {
-					201: z.object({
-						userId: z.number(),
-					}),
-				},
-			},
-		},
-		async (request: FastifyRequest, reply: FastifyReply) => {
-			const { email, name } = request.body;
+        response: {
+          201: z.object({
+            userId: z.number(),
+          }),
+        },
+      },
+    },
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const { email, name } = request.body;
 
-			const checkingIfTheUserAlreadyExists = await prisma.user.findUnique({
-				where: {
-					email,
-				},
-			});
+      const checkingIfTheUserAlreadyExists = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
 
-			if (checkingIfTheUserAlreadyExists !== null) {
-				return reply.status(409).send({ message: "User already registered" });
-			}
+      if (checkingIfTheUserAlreadyExists !== null) {
+        return reply.status(409).send({ message: "User already registered" });
+      }
 
-			const user = await prisma.user.create({
-				data: {
-					email,
-					name,
-				},
-			});
+      const user = await prisma.user.create({
+        data: {
+          email,
+          name,
+        },
+      });
 
-			return reply.status(201).send({ userId: user.id });
-		},
-	);
+      return reply.status(201).send({ userId: user.id });
+    },
+  );
 }
