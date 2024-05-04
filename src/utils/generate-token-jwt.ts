61f3c8a915a7a4248b6ed3bfb819ef02f829412f
Gabriel Moraes <121532661+angrycsm@users.noreply.github.com>
2024-05-04 00:11:55 +0000

    generate-token arrumed

--- src/utils/generate-token-jwt.ts
+++ src/utils/generate-token-jwt.ts
@@ -3,5 +3,5 @@ import jwt from "jsonwebtoken";
 const secretKey = process.env.JWT_SECRET
 
 export function generateToken(payload: any): string {
-  return jwt.verify(payload, secretKey, { expiresIn: '7d'}) 
+  return jwt.sign(payload, secretKey, { expiresIn: '7d'}) 
 }
